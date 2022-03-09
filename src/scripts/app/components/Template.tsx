d820c47864bb49bed93be534166f96d54f8781f4
harrygillingham99 <harrygillingham99@gmail.com>
2022-03-09 14:06:01 +0000

    feat(linter): added pre-commit hook and linters

--- src/scripts/app/components/Template.tsx
+++ src/scripts/app/components/Template.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
 export const Template : React.FC<{}> = () => {
-    return <p>This is a .NET 6 SPA Template!</p>
+    return <><p>This is a .NET 6 SPA Template!</p>
+    <p>Test Formatting</p></>
 }
